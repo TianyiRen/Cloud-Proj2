b53f3b5b3255879ca117e0eba7b2a19b208b7fd5
Xiaohu Chen <xc2263@columbia.edu>
2014-04-18 16:16:03 -0400

    add (at into stopwords

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ numticks = config.getint('View', 'number of ticks in timeline')
 
 stopwords = set([
 			'rt', 'today', "didn't", "don't", "[pic]", "i'm", "...", "..", "w/",
-			'&amp;',
+			'&amp;', "can't", "(at",
 			'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 
 			'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 
 			'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 
